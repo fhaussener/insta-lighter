b17aae06a7aa0463ba168a1d4f17a4e3da082b38
Haussener Fabio, GHR-SRT-NEX-C-1 <Fabio.Haussener@swisscom.com>
2020-04-10 17:46:56 +0200

    Use screenshot example

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
-const getProfileInfo = require('./instagram_account_profile');
+const getScreenshot = require('./screenshot');
+const { parse } = require('url')
 
 module.exports = async function (req, res) {
-  // const { pathname = '/', query = {} } = parse(req.url, true);
-  // let url = pathname.slice(1);
-
-  const file = await getProfileInfo("cristiano");
+  const { pathname = '/', query = {} } = parse(req.url, true);
+  const { type = 'png' } = query; // png or jpeg
+  let url = pathname.slice(1);
+  if (!url.startsWith('http')) {
+    url = 'https://' + url; // add protocol if missing
+  }
+  const file = await getScreenshot(url, type);
   res.statusCode = 200;
-  res.setHeader("Content-Type", `image`);
+  res.setHeader('Content-Type', `image/${type}`);
   res.end(file);
 };
--- api/instagram_account_profile.js
+++ api/instagram_account_profile.js
@@ -1,56 +1,56 @@
-const chrome = require('chrome-aws-lambda');
-const puppeteer = require('puppeteer-core');
+// const chrome = require('chrome-aws-lambda');
+// const puppeteer = require('puppeteer-core');
 
-async function getProfileInfo(account) {
+// async function getProfileInfo(account) {
 
-  const browser = await puppeteer.launch({
-    args: chrome.args,
-    executablePath: await chrome.executablePath,
-    headless: chrome.headless,
-  });
+//   const browser = await puppeteer.launch({
+//     args: chrome.args,
+//     executablePath: await chrome.executablePath,
+//     headless: chrome.headless,
+//   });
 
-  const page = await browser.newPage();
+//   const page = await browser.newPage();
 
-  // go to Instagram web profile (this example use Cristiano Ronaldo profile)
-  await page.goto('https://instagram.com/' + account);
+//   // go to Instagram web profile (this example use Cristiano Ronaldo profile)
+//   await page.goto('https://instagram.com/' + account);
 
-  // check username exists or not exists
-  let isUsernameNotFound = await page.evaluate(() => {
-    // check selector exists
-    if (document.getElementsByTagName('h2')[0]) {
-      // check selector text content
-      if (document.getElementsByTagName('h2')[0].textContent == "Sorry, this page isn't available.") {
-        return true;
-      }
-    }
-  });
+//   // check username exists or not exists
+//   let isUsernameNotFound = await page.evaluate(() => {
+//     // check selector exists
+//     if (document.getElementsByTagName('h2')[0]) {
+//       // check selector text content
+//       if (document.getElementsByTagName('h2')[0].textContent == "Sorry, this page isn't available.") {
+//         return true;
+//       }
+//     }
+//   });
 
-  if (isUsernameNotFound) {
-    console.log('Account not exists!');
+//   if (isUsernameNotFound) {
+//     console.log('Account not exists!');
 
-    // close browser
-    await browser.close();
-    return;
-  }
+//     // close browser
+//     await browser.close();
+//     return;
+//   }
 
-  // get username
-  let username = await page.evaluate(() => {
-    return document.querySelectorAll('header > section h2')[0].textContent;
-  });
+//   // get username
+//   let username = await page.evaluate(() => {
+//     return document.querySelectorAll('header > section h2')[0].textContent;
+//   });
 
 
-  // get username picture URL
-  let usernamePictureUrl = await page.evaluate(() => {
-    return document.querySelectorAll('header img')[0].getAttribute('src');
-  });
+//   // get username picture URL
+//   let usernamePictureUrl = await page.evaluate(() => {
+//     return document.querySelectorAll('header img')[0].getAttribute('src');
+//   });
 
-  // close the browser
-  await browser.close();
+//   // close the browser
+//   await browser.close();
 
-  return ({
-    'username': username,
-    'username_picture_url': usernamePictureUrl,
-  })
-}
+//   return ({
+//     'username': username,
+//     'username_picture_url': usernamePictureUrl,
+//   })
+// }
 
-module.exports = getProfileInfo;
+// module.exports = getProfileInfo;
