a0db57269b566227b5d77f2d535677c2f42eee13
Haussener Fabio, GHR-SRT-NEX-C-1 <Fabio.Haussener@swisscom.com>
2020-04-10 17:19:55 +0200

    Remove parse

--- api/index.js
+++ api/index.js
@@ -1,8 +1,8 @@
 const getProfileInfo = require('./instagram_account_profile');
 
 module.exports = async function (req, res) {
-  const { pathname = '/', query = {} } = parse(req.url, true);
-  let url = pathname.slice(1);
+  // const { pathname = '/', query = {} } = parse(req.url, true);
+  // let url = pathname.slice(1);
 
   const file = await getProfileInfo("cristiano");
   res.statusCode = 200;
