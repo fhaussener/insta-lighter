95b3a661b07abf9dc8e49bfe0eda32fe6df490a9
Haussener Fabio, GHR-SRT-NEX-C-1 <Fabio.Haussener@swisscom.com>
2020-04-12 16:25:05 +0200

    Should fucking work now

--- api/instagram.js
+++ api/instagram.js
@@ -1,6 +1,5 @@
 const chrome = require('chrome-aws-lambda');
 const puppeteer = require('puppeteer-core');
-const { parse } = require('url')
 
 // const isDev = process.env.NOW_REGION === 'dev1';
 
@@ -15,6 +14,7 @@ const { parse } = require('url')
 //   } else {
 //     options = {
 //       args: chrome.args,
+//       defaultViewport: chrome.defaultViewport,
 //       executablePath: await chrome.executablePath,
 //       headless: chrome.headless,
 //     };
@@ -23,20 +23,22 @@ const { parse } = require('url')
 // }
 
 async function getProfileInfo(req, res) {
-  const { pathname = '/', query = {} } = parse(req.url, true);
+  const pathname = req.url
   let urlFrag = pathname.split("/");
   let account = urlFrag[2];
 
   const browser = await puppeteer.launch({
     args: chrome.args,
+    defaultViewport: chrome.defaultViewport,
     executablePath: await chrome.executablePath,
     headless: chrome.headless,
   });
 
   const page = await browser.newPage();
-  page.setDefaultTimeout(0);
 
   await page.goto('https://instagram.com/' + account);
+  await page.waitForSelector('header > section h2');
+  await page.waitForSelector('header img');
 
   // check username exists or not exists
   let isUsernameNotFound = await page.evaluate(() => {
@@ -50,20 +52,17 @@ async function getProfileInfo(req, res) {
   });
 
   if (isUsernameNotFound) {
-    console.log('Account not exists!');
-
+    res.status(404).send(`username not found`);
     // close browser
     await browser.close();
     return;
   }
 
-  // get username
-  // let username = await page.evaluate(() => {
-  //   return document.querySelectorAll('header > section h2')[0].textContent;
-  // });
+  let username = await page.evaluate(() => {
+    return document.querySelectorAll('header > section h2')[0].textContent;
+  });
 
 
-  // get username picture URL
   let usernamePictureUrl = await page.evaluate(() => {
     return document.querySelectorAll('header img')[0].getAttribute('src');
   });
@@ -72,12 +71,12 @@ async function getProfileInfo(req, res) {
   await browser.close();
 
   const response = {
-    username: account,
-    username_picture_url: usernamePictureUrl,
+    username: username,
+    username_picture_url: usernamePictureUrl
   }
 
   res.setHeader('Content-Type', `application/json`);
-  res.status(200).json(response)
+  res.status(200).json(response);
 }
 
 module.exports = getProfileInfo;
