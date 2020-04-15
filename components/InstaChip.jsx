7ccd7bb92d37546bafeefec1e0363b90a8a172fc
Haussener Fabio, GHR-SRT-NEX-C-1 <Fabio.Haussener@swisscom.com>
2020-04-15 21:36:44 +0200

    Fix ios stuff

--- components/InstaChip.jsx
+++ components/InstaChip.jsx
@@ -5,7 +5,8 @@ const InstaChip = ({ onClose, username }) => {
   return (
     <div className={styles.container}>
       <span className={styles.close} onClick={onClose}></span>
-      <span className={styles.insta}>{username}</span>
+      <span className={styles.insta}></span>
+      <span>{username}</span>
     </div>
   );
 }
